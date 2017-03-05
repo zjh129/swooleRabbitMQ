7da485b1ad0b0e99bb02f6d98b3831e278ce476a
zhaojianhui <zhaojianhui129@163.com>
2017-03-05 17:33:12 +0800

    队列

---
+++ queue/controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+use Swoole;
+
+class Home extends Swoole\Controller
+{
+    public function __construct($swoole)
+    {
+        parent::__construct($swoole);
+        /*Swoole::$php->session->start();
+        Swoole\Auth::loginRequire();*/
+    }
+
+    public function index()
+    {
+        return time()."\n";
+    }
+}
